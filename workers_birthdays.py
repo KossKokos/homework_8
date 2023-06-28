e771e6e4f16b5e5821e82f650be03faffa07475f
KossKokos <koskokos0@gmail.com>
2023-06-28 12:56:24 +0100

    again

--- workers_birthdays.py
+++ workers_birthdays.py
@@ -1,18 +1,20 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 lst = [{'Kostia':datetime(2005, 6, 28)}, 
        {'Nastia': datetime(2005, 6, 30)}, 
-       {'Vasia': datetime(2005, 6, 27)}, 
+       {'Vasia': datetime(2005, 6, 5)}, 
        {'Daniel': datetime(2005, 6, 28)}, 
        {'Obama': datetime(2005, 7, 2 )}, 
-       {'Volodia': datetime(2000, 7, 1)},
-       {'Donald': datetime(2006, 7, 29)}]
+       {'Volodia': datetime(2000, 7, 6)},
+       {'Donald': datetime(2006, 7, 5)}]
 def get_birthdays_per_week(list_of_workers):
     lst_names = []
     dict_of_days = {}
     current_date = datetime.now()
     for dct in list_of_workers:
         for key, value in dct.items():
-            if current_date.month == value.month and not value.day - current_date.day < 0 and value.day - current_date.day <= 7:
+            birthday = datetime(year=current_date.year, month=value.month, day=value.day)
+            less_than_7 = birthday - current_date
+            if less_than_7.days >= -1 and less_than_7.days <= 6:
                 dict_of_dates = {key:datetime(year=current_date.year,month=value.month, day=value.day )}
                 lst_names.append(dict_of_dates)
             else:
@@ -28,8 +30,6 @@ def get_birthdays_per_week(list_of_workers):
             else:
                 dict_of_days.update({weekday: name})
              
-           
-   # print(lst_names)
     print(dict_of_days)
     _1 = ''
     _4 = ''
@@ -58,7 +58,6 @@ def get_birthdays_per_week(list_of_workers):
         print(f'{_4}: {dict_of_days[_4]}')
     if _5 == 'Friday':
         print(f'{_5}: {dict_of_days[_5]}')
-
 
 
 get_birthdays_per_week(lst)
